be3a6a0314fd058e8d8ae32599b834ffd2965339
ΛŁΞppФ_tΞΛm <karam@moakt.ws>
2016-12-17 11:00:28 +0000

    Create config.lua

---
+++ data/config.lua
@@ -0,0 +1,41 @@
+do local _ = { 
+  about_text = "▫️Welcome to Aleppo V2 For more information Subscribe to the channel @Aleppo_Team \n https://github.com/AleppoTeam/Aleppo \n\n▫️Devlopers @TH3_Developer \n @syria_ar \n @allith \n\n▫️ Dev Bot @Aleppo_Team_bot \n\n ▫️channel @Aleppo_Team ", 
+  enabled_plugins = { 
+    "@Aleppo_Team", 
+    "ar-anti_spam", 
+    "ar-banhammer", 
+    "ar-emoji", 
+    "ar-english", 
+    "ar-fwd", 
+    "ar-he1", 
+    "ar-he2", 
+    "ar-help", 
+    "ar-info", 
+    "ar-join", 
+    "ar-me", 
+    "ar-media", 
+    "ar-msg_checks", 
+    "ar-onservice", 
+    "ar-owners", 
+    "ar-redis", 
+    "ar-run1", 
+    "ar-sudo", 
+    "ar-supergroup", 
+    "ar-tag", 
+    "ar-username", 
+    "rebot", 
+    "VIRSON", 
+    "ar-add_sudo", 
+  }, 
+  help_text = "", 
+  help_text_realm = "", 
+  help_text_super = "", 
+  moderation = { 
+    data = "data/moderation.json" 
+  }, 
+  sudo_users = { 
+    0, ---Aleppo امسح الصفر وحط بداله ايديك Team 
+  } 
+} 
+return _ 
+end 
